7d28e63164e415c883ceba89d0db88968bddcf05
imen-imen <imensebtaoui136@gmail.com>
2021-06-25 01:23:23 +0100

    First Commit

---
+++ src/main/java/com/example/demo/Stage2021Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stage2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stage2021Application.class, args);
+	}
+
+}
